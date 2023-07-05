d3c2483edf440eaac9ef12c66c75d8084b8d450e
hhyowon <hyowon0605@gmail.com>
2023-07-05 12:05:48 +0900

    dd

--- docs/browser_js/carInfor/car.js
+++ docs/browser_js/carInfor/car.js
@@ -6,6 +6,7 @@ const carinfor_obj = [
     {"YEAR":"2020","CAR_NAME":"Camry","CAR_INFOR_ID":"CI005","COMPANY_ID":"C005"}
   ];
   
+
   let outHtml = `<table>`; //테이블열기
   for (let car_hashmap of carinfor_obj) {   
     // 각 자동차의 정보를 콘솔에 출력(올바르게 액세스하고 있는지 확인용)
@@ -18,6 +19,6 @@ const carinfor_obj = [
   console.log(outHtml); // outHtml 변수에 저장된 동적으로 생성된 HTML 코드를 콘솔에 기록
   
  // "carTableBody"라는 id를 가진 DOM 요소를 찾아서 동적으로 생성된 HTML을 추가
-  let carTableBody = document.querySelector('#carTableBody'); // list.html에서 이 id 넣음 <tbody id="carTableBody"> 
+  let carTableBody = document.querySelector('#carTableBody'); // list.html에서 이 id 넣음 <tbody id="carTableBody"> !!
   carTableBody.innerHTML = outHtml; //outHtml 변수에 저장된 동적으로 생성된 HTML이 선택된 <tbody> 요소의 내부 HTML로 할당됨
   
---
+++ docs/browser_js/carInfor/carinforwithfunction.js
@@ -0,0 +1,28 @@
+
+function tablelist(){
+
+    const carinfor_obj = [
+        {"YEAR":"2020","CAR_NAME":"Sonata","CAR_INFOR_ID":"CI001","COMPANY_ID":"C001"},
+        {"YEAR":"2021","CAR_NAME":"Kona","CAR_INFOR_ID":"CI002","COMPANY_ID":"C002"},
+        {"YEAR":"2019","CAR_NAME":"SM6","CAR_INFOR_ID":"CI003","COMPANY_ID":"C003"},
+        {"YEAR":"2022","CAR_NAME":"3 Series","CAR_INFOR_ID":"CI004","COMPANY_ID":"C004"},
+        {"YEAR":"2020","CAR_NAME":"Camry","CAR_INFOR_ID":"CI005","COMPANY_ID":"C005"}
+      ];
+    
+      let outHtml = `<table>`; 
+      for (let car_hashmap of carinfor_obj) {   
+        console.log(`YEAR: ${car_hashmap.YEAR}, CAR_NAME: ${car_hashmap.CAR_NAME}, CAR_INFOR_ID: ${car_hashmap.CAR_INFOR_ID}, COMPANY_ID: ${car_hashmap.COMPANY_ID}`);
+        outHtml += `<tr><td>${car_hashmap.YEAR}</td><td>${car_hashmap.CAR_NAME}</td><td>${car_hashmap.CAR_INFOR_ID}</td><td>${car_hashmap.COMPANY_ID}</td></tr>`;
+      }
+      outHtml += `</table>`; 
+      console.log(outHtml);
+    
+      let carTableBody = document.querySelector('#carTableBody'); 
+      carTableBody.innerHTML = outHtml;
+
+      return outHtml;
+}
+
+tablelist();
+
+
